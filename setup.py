e73e066e6d0c07b9e3d09ef6d996e0750a9d725a
Jimbito <joverend98@gmail.com>
2022-03-27 14:20:37 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from ensurepip import version
+from unicodedata import name
+from urllib.error import URLError
+from setuptools import setup
+from setuptools import find_packages
 
+setup(
+    name='letterboxd_scraper',
+    version='0.0.1',
+    description='Mock package that allows you to selectively scrape from the Letterboxd website.',
+    URL='https://github.com/Jimbito/Letterboxd_Web_Scraper.git',
+    author='James Overend',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests','selenium']
+)
